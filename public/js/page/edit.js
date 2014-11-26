8c5923cf5f282d453815c549b0a3ed2f09dcd6e5
悠歌 <guangyuan.cgy@alibaba-inc.com>
2014-11-26 10:30:35 +0800

    ui stuff

--- public/js/page/edit.js
+++ public/js/page/edit.js
@@ -1,11 +1,11 @@
 lovefly.controller('EditorController', function($scope, $http, $log, _) {
 
     if (location.href.indexOf("stamp") > 0) {
-      var urlParams = location.href.split("?")[1].split("&");
-      var urlParamsMap = {};
-      for (var i = urlParams.length - 1; i >= 0; i--) {
-          urlParamsMap[urlParams[i].split("=")[0]] = urlParams[i].split("=")[1];
-      };
+        var urlParams = location.href.split("?")[1].split("&");
+        var urlParamsMap = {};
+        for (var i = urlParams.length - 1; i >= 0; i--) {
+            urlParamsMap[urlParams[i].split("=")[0]] = urlParams[i].split("=")[1];
+        };
     };
     $scope.logError = function(data, status) {
         $log.log('code ' + status + ': ' + data);
@@ -80,73 +80,85 @@ lovefly.controller('EditorController', function($scope, $http, $log, _) {
             parentId: 1
         }, {
             id: 13,
-            name: '公司新闻',
-            parentId: 1
-        }, {
-            id: 13,
             name: '飞机介绍',
             parentId: 1
         }, {
-            id: 13,
-            name: '培训基础知识',
+            id: 14,
+            name: '航空英语',
             parentId: 1
         }, {
-            id: 13,
-            name: '航空英语',
+            id: 15,
+            name: '常见问题',
             parentId: 1
         }]
     }, {
         id: 2,
-        name: '培训',
+        name: '培训项目',
         children: [{
             id: 21,
-            name: '私人执照飞行培训',
+            name: '私人飞行执照',
             parentId: 2
         }, {
             id: 22,
-            name: '仪器仪表执照培训',
+            name: '仪器仪表执照',
             parentId: 2
         }, {
             id: 23,
-            name: '单引擎商业指导培训',
-            parentId: 2
+            name: '商业飞行执照',
+            parentId: 2,
+            children: [{
+                id: 231,
+                name: '单引擎商业飞行执照',
+                parentId: 23
+            }, {
+                id: 232,
+                name: '多引擎商业飞行执照',
+                parentId: 23
+            }]
         }, {
             id: 24,
-            name: '多引擎商业指导培训',
+            name: '多引擎飞行执照',
             parentId: 2
         }, {
             id: 25,
-            name: 'FAA/EASA机构认证指导培训',
-            parentId: 2
-        }, {
-            id: 26,
-            name: '飞机维护培训',
+            name: '飞行维护培训',
             parentId: 2
         }]
     }, {
         id: 3,
         name: '招生',
         children: [{
-            id: 31,
-            name: '学校招生报名页面',
+            id: 32,
+            name: '培训学校',
+            parentId: 3
+        }, {
+            id: 33,
+            name: '招生代理',
             parentId: 3
         }]
-    },
-    {
+    }, {
         id: 4,
-        name: '关于',
+        name: '俱乐部',
         children: [{
             id: 41,
-            name: '公司简介',
+            name: '服务宗旨',
             parentId: 4
         }, {
             id: 42,
-            name: '公司资质',
+            name: '会员特权',
             parentId: 4
         }, {
             id: 43,
-            name: '公司文化',
+            name: '合作加盟',
             parentId: 4
+        }]
+    }, {
+        id: 5,
+        name: '关于',
+        children: [{
+            id: 51,
+            name: '关于',
+            parentId: 5
         }]
     }];
 
@@ -155,7 +167,7 @@ lovefly.controller('EditorController', function($scope, $http, $log, _) {
             // $log.info($scope.islogin);
             if (!!urlParamsMap) {
                 $scope.stamp = urlParamsMap["stamp"];
-                this.getPost();//进入编辑文章流程
+                this.getPost(); //进入编辑文章流程
             };
         },
         getPost: function() {
@@ -169,8 +181,12 @@ lovefly.controller('EditorController', function($scope, $http, $log, _) {
                 if (!!data && data.length != 0 && data != "null") {
                     $scope.title = data.Title;
                     $scope.content = data.Content;
-                    $scope.categoryId = _.find($scope.categories, function(item){return item.id == data.CategoryId});
-                    $scope.subCategoryId = _.find($scope.categoryId.children, function(item){return item.id == data.SubCategoryId});
+                    $scope.categoryId = _.find($scope.categories, function(item) {
+                        return item.id == data.CategoryId
+                    });
+                    $scope.subCategoryId = _.find($scope.categoryId.children, function(item) {
+                        return item.id == data.SubCategoryId
+                    });
                 };
             }).
             error($scope.logError);
